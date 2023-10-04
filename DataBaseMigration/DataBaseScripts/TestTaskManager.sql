5f5b8715e098e32f1ad891a6043f32ff939febca
Siddhi17 <siddhi.shah@clariontech.com>
2023-10-04 12:38:42 +0530

    test script.

---
+++ DataBaseMigration/DataBaseScripts/TestTaskManager.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE [dbo].[GetTaskManagerDataALLTasks]
+AS
+BEGIN
+SELECT [TaskManagerId]
+      ,[Title]
+      ,[Description]
+      ,[Duedate]
+      ,[Status]
+FROM [dbo].[TaskManagers]
+END;
