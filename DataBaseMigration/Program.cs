3627f184edd8e9c248d30dcf60fc2dbea10e32eb
Siddhi17 <siddhi.shah@clariontech.com>
2023-10-04 16:17:43 +0530

    test

--- DataBaseMigration/Program.cs
+++ DataBaseMigration/Program.cs
@@ -8,24 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json")
+    .Build();
 if (args.Contains("--migrate", StringComparer.OrdinalIgnoreCase))
 {
-    var appSettingsPath = "appsettings.json"; // Assuming the file is in the same directory as the executable
-    Console.WriteLine($"Loading appsettings from: {appSettingsPath}");
-
-    var configuration = new ConfigurationBuilder()
-        .AddJsonFile(appSettingsPath)
-        .Build();
-
-    var connectionString = configuration.GetConnectionString("SqlDb");
-    Console.WriteLine($"Connection string: {connectionString}");
-
+    Console.WriteLine($"Connection string: {configuration}");
     DatabaseMigrationConfiguration.ConfigureAndMigrate(configuration);
-}
-
-else
-{
-    Console.WriteLine("Database migration skipped. To run the migration, use the --migrate command-line argument.");
 }
 
 var app = builder.Build();
