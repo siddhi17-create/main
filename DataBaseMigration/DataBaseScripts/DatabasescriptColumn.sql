aa072dc77d24a458177021f7e688ee6802b4040f
Siddhi17 <siddhi.shah@clariontech.com>
2023-10-04 17:47:02 +0530

    testsql

---
+++ DataBaseMigration/DataBaseScripts/DatabasescriptColumn.sql
@@ -0,0 +1,10 @@
+ALTER PROCEDURE [dbo].[GetTaskManagerDataALL]
+AS
+BEGIN
+SELECT [TaskManagerId]
+      ,[Title]
+      ,[Description]
+      ,[Duedate]
+      ,[Status]
+FROM [dbo].[TaskManagers] WHERE TaskManagerId = 1;
+END;
