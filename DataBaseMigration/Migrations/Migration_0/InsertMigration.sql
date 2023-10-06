a88bbb60f31e6ebe9423e2d4b4ea95f886cd6e82
siddhi shah <siddhi.shah@clariontech.com>
2023-10-07 01:18:22 +0530

    no sql script

--- DataBaseMigration/Migrations/Migration_0/InsertMigration.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE InsertMigrationHistory
-    @MigrationName NVARCHAR(255),
-    @AppliedOn DATETIME2
-AS
-BEGIN
-    INSERT INTO MigrationHistory (MigrationName, AppliedOn)
-    VALUES (@MigrationName, @AppliedOn);
-END;
